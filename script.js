880659103946396f27b14165be7ba00dd817bff2
Willian Faria <willian.betin.faria@gmail.com>
2023-10-21 23:31:29 -0300

    feat: changing avatars and alt photos

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function toggleMode() {
   const html = document.documentElement
-
   html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+
+  const alt = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("alt", "Teste Light")
+  } else {
+    img.setAttribute("alt", "Teste Dark")
+  }
 }
